cf689d4e94b2815943ab41bacb50744f3d39a5ed
saoyagi2 <saoyagi2@gmail.com>
2020-05-09 18:43:03 +0900

    HTMLパースとリンクチェック

--- linkchecker.pl
+++ linkchecker.pl
@@ -3,18 +3,34 @@
 use strict;
 use warnings;
 
+use HTML::Parser;
 use Data::Dumper;
 
 if(@ARGV < 1) {
   usage();
 }
-else {
-  linkcheck($ARGV[0]);
-}
 
-sub linkcheck {
-  my $basedir = shift;
-  my @files = list_files($basedir, '');
+my @links;
+my $basedir = $ARGV[0];
+my $parser = HTML::Parser->new(
+  api_version => 3,
+  start_h     => [
+    sub {
+      my ($tagname, $attr) = @_;
+      if($tagname eq 'a' && defined($attr->{'href'})) {
+        push @links, $attr->{'href'};
+      }
+    }, "tagname, attr"]),;
+
+foreach my $file (list_files($basedir, '')) {
+  @links = ();
+  $parser->parse_file("$basedir/$file");
+  foreach my $link (@links) {
+    next if $link =~ /^https?:/;
+    next if -f "$basedir/$link";
+    next if $link =~ /\/$/ && -f "$basedir/${link}index.html";
+    print "$basedir/$file: $link\n";
+  }
 }
 
 sub list_files {
@@ -25,10 +41,10 @@ sub list_files {
   while(my $file = readdir $dh) {
     next if $file =~ /^\./;
     if(-f "$basedir/$dir/$file") {
-      push @files, "$dir/$file";
+      push @files, ($dir ne '' ? "$dir/" : '') . $file;
     }
     if(-d "$basedir/$dir/$file") {
-      push @files, list_files($basedir, "$dir/$file");
+      push @files, list_files($basedir, ($dir ne '' ? "$dir/" : '') . $file);
     }
   }
 
