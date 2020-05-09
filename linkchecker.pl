ab077af88e6af6e0972f8d6d82b8bfe0bda77a11
saoyagi2 <saoyagi2@gmail.com>
2020-05-09 17:38:21 +0900

    ディレクトリ探索

--- linkchecker.pl
+++ linkchecker.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+use Data::Dumper;
+
 if(@ARGV < 1) {
   usage();
 }
@@ -12,6 +14,25 @@ else {
 
 sub linkcheck {
   my $basedir = shift;
+  my @files = list_files($basedir, '');
+}
+
+sub list_files {
+  my ($basedir, $dir) = @_;
+  my @files = ();
+
+  opendir my $dh, "$basedir/$dir" or die "Can't open directory $basedir/$dir: $!";
+  while(my $file = readdir $dh) {
+    next if $file =~ /^\./;
+    if(-f "$basedir/$dir/$file") {
+      push @files, "$dir/$file";
+    }
+    if(-d "$basedir/$dir/$file") {
+      push @files, list_files($basedir, "$dir/$file");
+    }
+  }
+
+  return @files;
 }
 
 sub usage {
